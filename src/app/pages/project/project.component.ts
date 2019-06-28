84e22245a51ed8497c107eae21999b21163b2d2f
Mustafa <mcay51@gmail.com>
2019-06-29 01:06:51 +0300

    değişken tanımlanıp değeri html sayfasında gösterildi

--- src/app/pages/project/project.component.ts
+++ src/app/pages/project/project.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectComponent implements OnInit {
 
+  projectMessage='Project Message!!';
   constructor() { }
 
   ngOnInit() {
